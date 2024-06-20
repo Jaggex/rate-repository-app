2aff69d87b984abcc055d68ecee7596eefe56d19
janne@bronex.fi <janne@bronex.fi>
2024-06-21 02:44:12 +0300

    10.24 prepping

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,10 +1,10 @@
 import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 
-const useRepositories = ({ orderBy, orderDirection }) => {
+const useRepositories = ({ orderBy, orderDirection, searchKeyword }) => {
   const { data, loading, refetch } = useQuery(GET_REPOSITORIES, {
+    variables: { orderBy, orderDirection, searchKeyword },
     fetchPolicy: 'cache-and-network',
-    variables: { orderBy, orderDirection },
   });
 
   const repositories = data ? data.repositories : null;
